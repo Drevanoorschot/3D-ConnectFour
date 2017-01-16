35b100fa3708d5cbf28e81de9022a1d756654ff4
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2017-01-16 17:03:11 +0100

    implement enum Mark

--- src/connect4/model/Mark.java
+++ src/connect4/model/Mark.java
@@ -1,5 +1,18 @@
 package connect4.model;
 
-public interface Mark {
-
+public enum Mark {
+	X, O, EMPTY;
+	public Mark next(Mark m) {
+		switch(m) {
+		case O:
+			return X;
+		case X:
+			return O;
+		default:
+			return EMPTY;
+		
+		}
+	}
 }
+
+
