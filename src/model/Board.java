7b6f5be395ce39c8043073604b01ee4297be66fb
User <User@DESKTOP-H6B0VEK>
2017-01-16 17:34:04 +0100

    finish deepcopy and reset methods

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,21 +1,31 @@
 package model;
 
 public class Board {
-	private String[][][] boardArray = new String[3][3][3];
-	private Mark mark;
-	
+	private Mark[][][] boardArray = new Mark[3][3][3];
+	private Mark m;
 	public Board() {
-		for (int x = 0; x < 3; x++) {
-			for (int y = 0; y < 3; y++) {
-				for (int z = 0; z < 3; z++) {
-					boardArray[x][y][z] = mark.EMPTY;
-				}
-			}
-		}
+		reset();
 	}
 	
 	public Board deepCopy() {
 		Board copy = new Board();
-		
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				for (int z = 0; z < 3; z++) {
+					copy.boardArray[x][y][z] = this.boardArray[x][y][z];
+				}
+			}
+		}
+		return copy;
+	}
+	
+	public void reset() {
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				for (int z = 0; z < 3; z++) {
+					boardArray[x][y][z] = m.EMPTY;
+				}
+			}
+		}
 	}
 }
