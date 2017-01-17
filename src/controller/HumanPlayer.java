78ac147c2866702655d3b98d60628e908b1fa7e7
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2017-01-17 17:08:04 +0100

    implement player and humanplayer

--- src/controller/HumanPlayer.java
+++ src/controller/HumanPlayer.java
@@ -1,5 +1,58 @@
 package controller;
 
-public class HumanPlayer {
+import java.util.Scanner;
+
+import model.Mark;
+
+public class HumanPlayer extends Player {
+		
+	public HumanPlayer(Mark m, String n) {
+		super(m, n);
+	}
+
+	@Override
+	public int[] makeMove() {
+		int[] coord = new int[3];
+		System.out.println(getName() + ", your turn!");
+		Scanner in = new Scanner(System.in);	
+		
+		System.out.println("Please enter a column number (starting at 0)\n");
+		coord[0] = -1;
+		while (coord[0] < 0) {
+			try {
+				String input = in.nextLine();
+				coord[0] = Integer.parseInt(input);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input, please provide a valid column number\n");
+				coord[0] = -1;
+			}
+		}
+		
+		System.out.println("Please enter a row number (starting at 0)\n");
+		coord[1] = -1;
+		while (coord[1] < 0) {
+			try {
+				String input = in.nextLine();
+				coord[1] = Integer.parseInt(input);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input, please provide a valid row number\n");
+				coord[1] = -1;
+			}
+		}
+		
+		System.out.println("Please enter a height number (starting at 0)\n");
+		coord[2] = -1;
+		while (coord[2] < 0) {
+			try {
+				String input = in.nextLine();
+				coord[2] = Integer.parseInt(input);
+			} catch(NumberFormatException e) {
+				System.out.println("Invalid input, please provide a valid column number\n");
+				coord[2] = -1;
+			}
+		}
+		in.close();
+		return coord;
+	}
 
 }
