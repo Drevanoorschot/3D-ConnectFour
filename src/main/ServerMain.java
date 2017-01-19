8e1da7f8fe311ad3ab4fbd80f6f639350af28821
RaureanuAndrei <raureanuandrei@gmail.com>
2017-01-19 16:32:21 +0100

    added checking methods to servermethods class, added starting variables for server

--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
 	public static void main(String[] args) {
-		
+		 int port = 0;
+		 ServerSocket sock = null;
+		 Socket socketpair = null;
 	}
 }
--- src/main/ServerMethods.java
+++ src/main/ServerMethods.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import exceptions.InvalidInputException;
 
 public class ServerMethods implements Protocol {
@@ -8,7 +12,28 @@ public class ServerMethods implements Protocol {
 			throw new InvalidInputException("Usage: <port>");
 		}
 	}
-	public void checkPort(String port) {
-		if ()
+
+	 public void checkPort(String port) {
+		try {
+			Integer.parseInt(port);
+		} catch (NumberFormatException e) {
+			System.out.println("Port should be given as an integer");
+		}
 	}
+	 
+	 public void checkServerSocket(ServerSocket sock, int port) {
+		 try {
+			 sock = new ServerSocket(port);
+		 } catch (IOException e) {
+			 System.out.println("IOException has occurred");
+		 }
+	 }
+	 
+	 public void checkSocketPair(Socket socketpair, ServerSocket sock) {
+		 try {
+			 socketpair = sock.accept();
+		 } catch (IOException e) {
+			 System.out.println("IOException has occurred");
+		 }
+	 }
 }
