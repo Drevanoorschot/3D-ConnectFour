5465e9df5406a504705400d589e76146582d1bc6
RaureanuAndrei <raureanuandrei@gmail.com>
2017-01-19 14:21:45 +0100

    create invalidinputexception

---
+++ src/exceptions/InvalidInputException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class InvalidInputException extends Exception {
+	private static final long serialVersionUID = -4346322581772139027L;
+
+	public InvalidInputException() {
+		super("Invalid input!");
+	}
+}
