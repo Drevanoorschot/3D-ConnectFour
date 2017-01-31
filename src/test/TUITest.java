95b59e5c8b5c9670a923f9c7d9ff205de20bd767
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2017-01-31 14:26:18 +0100

    checkstyle fix in JAVADOC

--- src/test/TUITest.java
+++ src/test/TUITest.java
@@ -9,6 +9,8 @@ public class TUITest {
 		Board board = new Board();
 		TUI tui = new TUI(board);
 		board.setField(1, 2, Mark.O);
+		board.setField(1, 2, Mark.X);
+		board.setField(0, 0, Mark.X);
 		System.out.println(tui.boardToString(board));
 	}
 }
